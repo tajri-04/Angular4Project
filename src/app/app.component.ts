d4f722b5ccd89c0c0b6a13f82244b3c559504e9a
tajri <fouad.tajri@netapsys.fr>
2018-02-23 20:32:08 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { UsersService } from './users/users.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'MessageBoard';
+  constructor (public usersService: UsersService) {
+  }
+  ngOnInit() {
+    this.usersService.getLocation();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { appRoutes } from './app.route';
+import { UsersModule } from './users/users.module';
+import { MessagesModule } from './messages/messages.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    UsersModule,
+    BrowserModule,
+    FormsModule,
+    MessagesModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
